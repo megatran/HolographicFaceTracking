8f59ab897ebb3902b16eec043ff936a62bfa6d18
Nhan Tran <nhantrantnt@gmail.com>
2018-03-17 18:14:07 -0600

    finished implementing framegrabber

--- FaceTag/Content/FrameGrabber.cs
+++ FaceTag/Content/FrameGrabber.cs
@@ -12,15 +12,67 @@ namespace FaceTag.Content
 {
     class FrameGrabber
     {
+        public struct Frame
+        {
+            // Simple data object that holds reference to the latest arrived frame for later analysis
+            public MediaFrameReference mediaFrameReference;
+            public SpatialCoordinateSystem spatialCoordinateSystem;
+            public CameraIntrinsics cameraIntrinsics;
+            public long timestamp;
+
+        }
+
+        //class attributes
         private MediaCapture mediaCapture;
         private MediaFrameSource mediaFrameSource;
         private MediaFrameReader mediaFrameReader;
 
+        private Frame _lastFrame;
+
+        public Frame LastFrame
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _lastFrame;
+                }
+            }
+            private set
+            {
+                lock(this)
+                {
+                    _lastFrame = value;
+                }
+            }
+        }
+
+        private DateTime _lastFrameCapturedTimeStamp = DateTime.MaxValue;
+
+        public float ElapseTimeSinceLastFrameCaptured
+        {
+            get
+            {
+                return (float)(DateTime.Now - DateTime.MinValue).TotalMilliseconds;
+            }
+        }
+
+        public bool IsValid
+            // determine whether FrameGrabber is ready to avoid having other classes trying to pull frames before FrameGrabber is ready
+        {
+            get
+            {
+                return mediaFrameReader != null;
+            }
+        }
+
         private FrameGrabber(MediaCapture mediaCapture = null, MediaFrameSource mediaFrameSource = null, MediaFrameReader mediaFrameReader - null)
         {
+
             this.mediaCapture = mediaCapture; // capture audio, video , and image from camera
             this.mediaFrameSource = mediaFrameSource; // source of camera fames (color camera in this case)
             this.mediaFrameReader = mediaFrameReader; // access to frames from a MediaFrameSource then notifies when new frame arrives
+
 
             if (this.mediaFrameReader != null)
             {
@@ -28,10 +80,7 @@ namespace FaceTag.Content
             }
         }
 
-        private void MediaFrameReader_FrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
-        {
-            throw new NotImplementedException();
-        }
+
 
         public static async Task<FrameGrabber> CreateAsync()
         {
@@ -87,6 +136,43 @@ namespace FaceTag.Content
 
             // ensure MediaFrameReader is successfully created to instantiate Grabber instance
             MediaFrameReaderStartStatus status = await mediaFrameReader.StartAsync();
+
+            if (status == MediaFrameReaderStartStatus.Success)
+            {
+                return new FrameGrabber(mediaCapture, selectedSource, mediaFrameReader);
+            } else
+            {
+                return new FrameGrabber();
+            }
+        }
+
+        void SetFrame(MediaFrameReference frame)
+            // extract meta data from the captured frame and update the LastFrame property
+        {
+            var spatialCoordinateSystem = frame.CoordinateSystem;
+            var cameraIntrinsics = frame.VideoMediaFrame.CameraIntrinsic;
+
+            // mediaFrameReference (capturedFrame) // include location of camera & perspective projection 
+            // of camera (coordiateSystem and CameraIntrinsic) to infer position of 
+            // the camera in real world and augment it with digital content
+            LastFrame = new Frame
+            {
+                mediaFrameReference = frame, 
+                spatialCoordinateSystem = spatialCoordinateSystem,
+                cameraIntrinsics = cameraIntrinsics,
+                timestamp = Utils.GetCurrentUnixTimestampMillis()
+            };
+
+            _lastFrameCapturedTimeStamp = DateTime.Now;
+        }
+
+        private void MediaFrameReader_FrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+        {
+            MediaFrameReference frame = sender.TryAcquireLatestFrame();
+            if (frame != null && frame.CoordinateSystem != null )
+            {
+                SetFrame(frame);
+            }
         }
     }
 }
